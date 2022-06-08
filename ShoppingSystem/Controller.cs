caae55d804ffcff51110c7c3eb74e69004796b8e
Николай Дубалов <nd48874338@edu.mon.bg>
2022-06-08 23:30:19 +0300

    Add project files.

---
+++ ShoppingSystem/Controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingSystem;
+
+namespace ShoppingSystem
+{
+    public class Controller
+    {
+        public Controller()
+        {
+        }
+        public string ProcessProductCommand(List<string> args)
+        {
+            PhysicalProduct product = new PhysicalProduct(args[0], double.Parse(args[1]), double.Parse(args[2]));
+            return product.ToString(); 
+        }
+        public string ProcessServiceCommand(List<string> args)
+        {
+            ServiceProduct product = new ServiceProduct(args[0], double.Parse(args[1]), double.Parse(args[2]));
+            return product.ToString();
+        }
+        public string ProcessCheckoutCommand(List<string> args)
+        {
+            Receipt receipt = new Receipt(args[0]);
+            return null;
+        }
+        public string ProcessInfoCommand(List<string> args)
+        {
+            
+            return null;
+        }
+        public string ProcessEndCommand()
+        {
+            
+            return null;
+        }
+
+    }
+}
---
+++ ShoppingSystem/Receipt.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSystem
+{
+    public class Receipt
+    {
+        private string customerName;
+        List<Product> products;
+
+        public string CustomerName
+        {
+            get { return customerName; }
+            private set
+            {
+                if (value.Length < 2 || value.Length > 40)
+                {
+                    throw new ArgumentOutOfRangeException("Name must be between 2 and 40 characters!");
+                }
+                customerName = value;
+            }
+        }
+
+        public Receipt(string customerName)
+        {
+            CustomerName = customerName;
+            products = new List<Product>();
+        }
+
+        public double productsSum { get => this.products.Sum(p => p.Price); }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Receipt of {CustomerName}");
+            sb.AppendLine($"Total Price: {this.productsSum:f2}");
+            sb.Append("Products:");
+            foreach (var prod in this.products)
+            {
+                sb.AppendLine();
+                sb.Append(prod.ToString());
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+    }
+}
